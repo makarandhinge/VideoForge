501510ab8f40fe29bd708c6bba05a0a9a1f42895
Makarand Milind Hinge <mmh2212002@gmail.com>
2025-12-07 20:07:49 +0530

    First Commit

---
+++ user-service/src/main/java/com/makarand/videoforge/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.makarand.videoforge.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
